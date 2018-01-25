64899b3a078e6e942772f21463900572a483f808
Joseph Surin <joseph.surin@gmail.com>
2018-01-25 15:48:19 +0800

    main layout and styles

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,13 @@
 $(document).ready(function() {
+    var animElements = $('.invisible')
+        $(document).scroll(function() {
+        for(var i = 0; i < animElements.length; i++) {
+            var offsetY = window.pageYOffset
+            var reqOffset = animElements.eq(i)[0].offsetTop-250
+            if(offsetY > reqOffset) {
+                animElements.eq(i).removeClass('invisible')
+                animElements.eq(i).addClass('fadeUp')
+            }
+        }
+    })
 })

94e696bfca8ccc0285687437069cb7fe06d3b177
Joseph Surin <joseph.surin@gmail.com>
2018-01-25 16:57:17 +0800

    change fonts and add js

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,36 @@
 $(document).ready(function() {
     var animElements = $('.invisible')
+    if(window.pageYOffset > 200) {
+        window.scrollBy(0, 1)
+    }
         $(document).scroll(function() {
         for(var i = 0; i < animElements.length; i++) {
             var offsetY = window.pageYOffset
-            var reqOffset = animElements.eq(i)[0].offsetTop-250
+            var reqOffset = animElements.eq(i)[0].offsetTop-(window.innerHeight-50)
             if(offsetY > reqOffset) {
                 animElements.eq(i).removeClass('invisible')
-                animElements.eq(i).addClass('fadeUp')
+                animElements.eq(i).addClass(animElements.eq(i).attr('anim'))
             }
         }
+    })
+    $('.shousai-button').click(function() {
+        $('html, body').animate({
+            scrollTop: $('.info-wrapper')[0].offsetTop-50
+        }, 1000)
+    })
+    $('.back-up').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000, function() {
+            for(var i = 0; i < animElements.length; i++) {
+                animElements.eq(i).removeClass(animElements.eq(i).attr('anim'))
+                animElements.eq(i).addClass('invisible')
+            }
+        })
+    })
+    $('.long-scroll').click(function() {
+        $('html, body').animate({
+            scrollTop: $(document).height()
+        }, 2000)
     })
 })
